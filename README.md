129153256c911a390c458b9107ee1419e9a8fb96
vietnguyen2000 <nguyen.viet.21042000@gmail.com>
2021-09-19 09:48:08 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Assignment Web - 211
+Make sure you have installed php:
+https://www.php.net/manual/en/install.php
+
+To start development:
+```
+php -S localhost:9001
+```
---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <link rel="icon" type="image/png" href="/assets/images/favicon.png"/>
+  <title>PHP Test</title>
+ </head>
+ <body>
+  <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
