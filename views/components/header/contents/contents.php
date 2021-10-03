dc54f1fcca539748b91e45f83c15605396322c15
vietnguyen2000 <nguyen.viet.21042000@gmail.com>
2021-10-03 22:01:05 +0700

    add search bar

--- views/components/header/contents/contents.php
+++ views/components/header/contents/contents.php
@@ -1,5 +1,8 @@
-<div class="px-5 d-flex justify-content-between header-bg py-3">
-  <?php 
-    require 'logo.php';
+<nav class="navbar px-5 justify-content-between header-bg py-3">
+  <?php
+  require 'logo.php';
+  require 'searchBar.php';
+
   ?>
-</div>
+  <div>a</div>
+</nav>
---
+++ views/components/header/contents/searchBar.php
@@ -0,0 +1,19 @@
+<!-- <form class="my-0 col-6">
+  <div class="row">
+    <div class="col-10 ps-0 pe-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    </div>
+    <div class="col-2 ps-0 pe-0">
+      <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
+    </div>
+  </div>
+</form> -->
+
+<div class="col-6">
+  <div class="input-group">
+    <input type="text" class="form-control" placeholder="Search" aria-label="search" aria-describedby="Search">
+    <div class="input-group-append">
+      <button class="btn btn-primary" type="button">Button</button>
+    </div>
+  </div>
+</div>
