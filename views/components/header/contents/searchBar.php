0772e1c809145e0d50b8a10e833681ca91eda660
vietnguyen2000 <nguyen.viet.21042000@gmail.com>
2021-10-04 05:21:19 +0700

    update search bar

--- views/components/header/contents/searchBar.php
+++ views/components/header/contents/searchBar.php
@@ -1,19 +1,12 @@
-<!-- <form class="my-0 col-6">
-  <div class="row">
-    <div class="col-10 ps-0 pe-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    </div>
-    <div class="col-2 ps-0 pe-0">
-      <button class="btn btn-primary my-2 my-sm-0" type="submit">Search</button>
-    </div>
-  </div>
-</form> -->
-
 <div class="col-6">
   <div class="input-group">
     <input type="text" class="form-control" placeholder="Search" aria-label="search" aria-describedby="Search">
     <div class="input-group-append">
-      <button class="btn btn-primary" type="button">Button</button>
+      <button class="btn btn-primary btn-search" type="button">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+      </button>
     </div>
   </div>
 </div>
