c97287f461cbb6546983a29a05e4ef5eac4d1655
Do Lam Truong <truong.do2304@hcmut.edu.vn>
2021-10-11 03:19:35 +0700

    watchPreview

---
+++ views/components/watchPreview/rawPreview.php
@@ -0,0 +1,132 @@
+
+<h3 class="NameWrap">Sản phẩm bán chạy</h3>
+<ul class="watch-grid-container">
+    <li>
+        <div class="watch-grid-item blackBoder">
+            <div class="imageWatchPreview">
+                <a href="">
+                    <img src="https://purepng.com/public/uploads/large/wrist-watch-ogx.png" alt="rolex" class="watch-grid-image">
+                </a>
+            </div>
+            <div class="watch-grid-info blackBoder">
+                <div class="center"><a href="" class="watchName">Rolex</a></div>
+                <div class="watchPrice">700,000 VND</div>
+                <div class="center"><button class="cartButton">Thêm vào giỏ </button></div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="watch-grid-item blackBoder">
+            <div class="imageWatchPreview">
+                <a href="">
+                    <img src="https://purepng.com/public/uploads/large/wrist-watch-ogx.png" alt="rolex" class="watch-grid-image">
+                </a>
+            </div>
+            <div class="watch-grid-info blackBoder">
+                <div class="center"><a href="" class="watchName">Rolex</a></div>
+                <div class="watchPrice">700,000 VND</div>
+                <div class="center"><button class="cartButton">Thêm vào giỏ </button></div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="watch-grid-item blackBoder">
+            <div class="imageWatchPreview">
+                <a href="">
+                    <img src="https://purepng.com/public/uploads/large/wrist-watch-ogx.png" alt="rolex" class="watch-grid-image">
+                </a>
+            </div>
+            <div class="watch-grid-info blackBoder">
+                <div class="center"><a href="" class="watchName">Rolex</a></div>
+                <div class="watchPrice">700,000 VND</div>
+                <div class="center"><button class="cartButton">Thêm vào giỏ </button></div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="watch-grid-item blackBoder">
+            <div class="imageWatchPreview">
+                <a href="">
+                    <img src="https://purepng.com/public/uploads/large/wrist-watch-ogx.png" alt="rolex" class="watch-grid-image">
+                </a>
+            </div>
+            <div class="watch-grid-info blackBoder">
+                <div class="center"><a href="" class="watchName">Rolex</a></div>
+                <div class="watchPrice">700,000 VND</div>
+                <div class="center"><button class="cartButton">Thêm vào giỏ </button></div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="watch-grid-item blackBoder">
+            <div class="imageWatchPreview">
+                <a href="">
+                    <img src="https://purepng.com/public/uploads/large/wrist-watch-ogx.png" alt="rolex" class="watch-grid-image">
+                </a>
+            </div>
+            <div class="watch-grid-info blackBoder">
+                <div class="center"><a href="" class="watchName">Rolex</a></div>
+                <div class="watchPrice">700,000 VND</div>
+                <div class="center"><button class="cartButton">Thêm vào giỏ </button></div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="watch-grid-item blackBoder">
+            <div class="imageWatchPreview">
+                <a href="">
+                    <img src="https://purepng.com/public/uploads/large/wrist-watch-ogx.png" alt="rolex" class="watch-grid-image">
+                </a>
+            </div>
+            <div class="watch-grid-info blackBoder">
+                <div class="center"><a href="" class="watchName">Rolex</a></div>
+                <div class="watchPrice">700,000 VND</div>
+                <div class="center"><button class="cartButton">Thêm vào giỏ </button></div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="watch-grid-item blackBoder">
+            <div class="imageWatchPreview">
+                <a href="">
+                    <img src="https://purepng.com/public/uploads/large/wrist-watch-ogx.png" alt="rolex" class="watch-grid-image">
+                </a>
+            </div>
+            <div class="watch-grid-info blackBoder">
+                <div class="center"><a href="" class="watchName">Rolex</a></div>
+                <div class="watchPrice">700,000 VND</div>
+                <div class="center"><button class="cartButton">Thêm vào giỏ </button></div>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="watch-grid-item blackBoder">
+            <div class="imageWatchPreview">
+                <a href="">
+                    <img src="https://purepng.com/public/uploads/large/wrist-watch-ogx.png" alt="rolex" class="watch-grid-image">
+                </a>
+            </div>
+            <div class="watch-grid-info blackBoder">
+                <div class="center"><a href="" class="watchName">Rolex</a></div>
+                <div class="watchPrice">700,000 VND</div>
+                <div class="center"><button class="cartButton">Thêm vào giỏ </button></div>
+            </div>
+        </div>
+    </li>
+</ul>
+
+<!-- <div class="watch-grid-container">
+  <div class="watch-grid-item"> 
+      <div class = "watch-grid-image"><img src="https://purepng.com/public/uploads/large/wrist-watch-ogx.png" alt="rolex"></div>
+      <div class = "watch-grid-info">   </div>
+  </div>
+  <div class="watch-grid-item">2</div>
+  <div class="watch-grid-item">3</div>  
+  <div class="watch-grid-item">4</div>
+  <div class="watch-grid-item">5</div>
+  <div class="watch-grid-item">6</div>  
+  <div class="watch-grid-item">7</div>
+  <div class="watch-grid-item">8</div>
+  <div class="watch-grid-item">9</div>  
+  
+</div> -->
