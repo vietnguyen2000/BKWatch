e88ab171ee470a60f8c75ae48cd85d84ba80f76c
vietnguyen2000 <nguyen.viet.21042000@gmail.com>
2021-10-25 22:02:19 +0700

    add register

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace Controllers;
 
+use Models\UserModel;
+use Views\ErrorView;
 use Views\UserView;
 
 class UserController
@@ -10,5 +12,38 @@ class UserController
   {
     $view = new UserView();
     $view->render(['url' => $url, 'nav' => '/blog']);
+  }
+
+  public function register($url)
+  {
+    $user = new UserModel();
+    $conditions = [
+      ["username" => $_POST["username"]],
+      ["email" => $_POST["email"]]
+    ];
+    $existUser = $user->getByCondition(...$conditions);
+    if (count($existUser)) {
+      $errorView = new ErrorView();
+      $errorView->render([
+        'errorCode' => '406',
+        'title' => "Tồn tại email hoặc tên đăng nhập",
+        'text' => "Email hoặc tên đăng nhập của bạn đã tồn tại. Xin hãy thử lại!"
+      ]);
+      return;
+    };
+
+    $data = [
+      'username' => $_POST['username'],
+      'password' => password_hash($_POST['password'], null),
+      'fullname' => $_POST['fullname'],
+      'email' => $_POST['email'],
+      'phoneNumber' => $_POST['phoneNumber'],
+      'gender' => intval($_POST['gender']),
+      'role' => 1,
+    ];
+
+
+    print_r($_POST);
+    $user->insert($data);
   }
 }
