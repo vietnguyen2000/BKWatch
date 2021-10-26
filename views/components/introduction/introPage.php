b9b632f1bf0cafc9275e18598efe96b1d7c76c7a
LinhBKU <linh.huynh.dev.99@gmail.com>
2021-10-26 07:27:24 +0700

    xoa card view css

--- views/components/introduction/introPage.php
+++ views/components/introduction/introPage.php
@@ -8,133 +8,134 @@
 	?>
 </style>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<div>
-	<div class=".container-xl">
-		<div class="row justify-content-between">
-			<div class="col-12 col-sm-12 col-md-6">
-				<div>
-					<div class="image-wrapper">
-						<img src="https://i.ytimg.com/vi/vhXiB8-NxGs/maxresdefault.jpg" class="img-banner" alt="...">
-					</div>
-				</div>
-			</div>
-			<div class="col-12 col-sm-12 col-md-6">
-				<div class="intro-content">
+<div class="container">
+	<div>
+		<div class=".container-xl">
+			<div class="row justify-content-between">
+				<div class="col-12 col-sm-12 col-md-6">
 					<div>
-						<h1>Giới thiệu về BKWATCH</h1>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultrices sit amet ipsum vel luctus. Suspendisse eu libero felis. Sed eu malesuada odio. Sed facilisis sagittis mi et ornare. Vivamus finibus mollis nunc quis vestibulum. Pellentesque tempus blandit dictum. Nulla sem nibh, pretium in urna a, tristique ultrices felis. Morbi vel justo mi. Sed vel lobortis tortor. Nulla interdum nibh vel urna vulputate, sit amet viverra nunc tincidunt.</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<div>
-	<div class=".container-xl">
-		<div class="row justify-content-between">
-			<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-				<div class="card-view">
-					<div class="card card-contain">
-						<div class="row justify-content-between">
-							<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-								<i class="material-icons md-96">watch</i>
-							</div>
-							<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-								</div>
-							</div>
+						<div class="image-wrapper">
+							<img src="https://i.ytimg.com/vi/vhXiB8-NxGs/maxresdefault.jpg" class="img-banner" alt="...">
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-				<div class="card-view">
-					<div class="card card-contain">
-						<div class="row justify-content-between">
-							<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-								<i class="material-icons md-96">watch</i>
-							</div>
-							<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-				<div class="card-view">
-					<div class="card card-contain">
-						<div class="row justify-content-between">
-							<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-								<i class="material-icons md-96">watch</i>
-							</div>
-							<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-				<div class="card-view">
-					<div class="card card-contain">
-						<div class="row justify-content-between">
-							<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-								<i class="material-icons md-96">watch</i>
-							</div>
-							<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-				<div class="card-view">
-					<div class="card card-contain">
-						<div class="row justify-content-between">
-							<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-								<i class="material-icons md-96">watch</i>
-							</div>
-							<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-				<div class="card-view">
-					<div class="card card-contain">
-						<div class="row justify-content-between">
-							<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-								<i class="material-icons md-96">watch</i>
-							</div>
-							<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-								<div class="card-body">
-									<h5 class="card-title">Card title</h5>
-									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-								</div>
-							</div>
+				<div class="col-12 col-sm-12 col-md-6">
+					<div class="intro-content">
+						<div>
+							<h1>Giới thiệu về BKWATCH</h1>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultrices sit amet ipsum vel luctus. Suspendisse eu libero felis. Sed eu malesuada odio. Sed facilisis sagittis mi et ornare. Vivamus finibus mollis nunc quis vestibulum. Pellentesque tempus blandit dictum. Nulla sem nibh, pretium in urna a, tristique ultrices felis. Morbi vel justo mi. Sed vel lobortis tortor. Nulla interdum nibh vel urna vulputate, sit amet viverra nunc tincidunt.</p>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+	<div>
+		<div class=".container-xl">
+			<div class="row justify-content-between">
+				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
+					<div class="card-view">
+						<div class="card card-contain">
+							<div class="row justify-content-between">
+								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
+									<i class="material-icons md-96">watch</i>
+								</div>
+								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
+					<div class="card-view">
+						<div class="card card-contain">
+							<div class="row justify-content-between">
+								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
+									<i class="material-icons md-96">watch</i>
+								</div>
+								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
+					<div class="card-view">
+						<div class="card card-contain">
+							<div class="row justify-content-between">
+								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
+									<i class="material-icons md-96">watch</i>
+								</div>
+								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
+					<div class="card-view">
+						<div class="card card-contain">
+							<div class="row justify-content-between">
+								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
+									<i class="material-icons md-96">watch</i>
+								</div>
+								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
+					<div class="card-view">
+						<div class="card card-contain">
+							<div class="row justify-content-between">
+								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
+									<i class="material-icons md-96">watch</i>
+								</div>
+								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
+					<div class="card-view">
+						<div class="card card-contain">
+							<div class="row justify-content-between">
+								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
+									<i class="material-icons md-96">watch</i>
+								</div>
+								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-<div></div>
