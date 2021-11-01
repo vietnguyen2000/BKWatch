d3e383d7a8a3cc56d1afd0a560f1f03c19f5ff57
LinhBKU <linh.huynh.dev.99@gmail.com>
2021-11-01 13:43:57 +0700

    fix size intro page

---
+++ views/components/introduction/cvIntroPage.php
@@ -0,0 +1,13 @@
+<div class="row cvIntroPage">
+  <div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 left-body">
+    <div class="left-cv">
+      <i class="material-icons md-96"><?php echo $icon ?></i>
+    </div>
+  </div>
+  <div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9 right-body">
+    <div>
+      <p class="cv-body-title"><?php echo $title ?></p>
+      <p class="cv-body-des"><?php echo $des ?></p>
+    </div>
+  </div>
+</div>
--- views/components/introduction/introPage.php
+++ views/components/introduction/introPage.php
@@ -1,141 +1,85 @@
 <style>
-	<?php
-	ob_start();
-	include 'introPage.css';
-	$file_content = ob_get_contents();
-	ob_end_clean();
-	echo $file_content
-	?>
+  <?php
+  ob_start();
+  include 'introPage.css';
+  $file_content = ob_get_contents();
+  ob_end_clean();
+  echo $file_content
+  ?>
 </style>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <div class="container">
-	<div>
-		<div class=".container-xl">
-			<div class="row justify-content-between">
-				<div class="col-12 col-sm-12 col-md-6">
-					<div>
-						<div class="image-wrapper">
-							<img src="https://i.ytimg.com/vi/vhXiB8-NxGs/maxresdefault.jpg" class="img-banner" alt="...">
-						</div>
-					</div>
-				</div>
-				<div class="col-12 col-sm-12 col-md-6">
-					<div class="intro-content">
-						<div>
-							<h1>Giới thiệu về BKWATCH</h1>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultrices sit amet ipsum vel luctus. Suspendisse eu libero felis. Sed eu malesuada odio. Sed facilisis sagittis mi et ornare. Vivamus finibus mollis nunc quis vestibulum. Pellentesque tempus blandit dictum. Nulla sem nibh, pretium in urna a, tristique ultrices felis. Morbi vel justo mi. Sed vel lobortis tortor. Nulla interdum nibh vel urna vulputate, sit amet viverra nunc tincidunt.</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div>
-		<div class=".container-xl">
-			<div class="row justify-content-between">
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-					<div class="card-view">
-						<div class="card card-contain">
-							<div class="row justify-content-between">
-								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-									<i class="material-icons md-96">watch</i>
-								</div>
-								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-					<div class="card-view">
-						<div class="card card-contain">
-							<div class="row justify-content-between">
-								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-									<i class="material-icons md-96">watch</i>
-								</div>
-								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-					<div class="card-view">
-						<div class="card card-contain">
-							<div class="row justify-content-between">
-								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-									<i class="material-icons md-96">watch</i>
-								</div>
-								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-					<div class="card-view">
-						<div class="card card-contain">
-							<div class="row justify-content-between">
-								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-									<i class="material-icons md-96">watch</i>
-								</div>
-								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-					<div class="card-view">
-						<div class="card card-contain">
-							<div class="row justify-content-between">
-								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-									<i class="material-icons md-96">watch</i>
-								</div>
-								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-12 col-sm-6 col-md-6 col-lg-4">
-					<div class="card-view">
-						<div class="card card-contain">
-							<div class="row justify-content-between">
-								<div class="col-3 col-sm-4 col-md-3 col-lg-4 col-xl-3 img-card-view">
-									<i class="material-icons md-96">watch</i>
-								</div>
-								<div class="col-9 col-sm-12 col-md-9 col-lg-8 col-xl-9">
-									<div class="card-body">
-										<h5 class="card-title">Card title</h5>
-										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="top-body">
+    <div class="row justify-content-between">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-5 left-top-body">
+        <div>
+          <div class="image-wrapper">
+            <img src="https://i.ytimg.com/vi/vhXiB8-NxGs/maxresdefault.jpg" class="img-banner" alt="...">
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-sm-12 col-md-12 col-lg-7 right-top-body">
+        <div class="intro-content">
+          <div>
+            <h3>Giới thiệu về BKWATCH</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ultrices sit amet ipsum vel luctus. Suspendisse eu libero felis. Sed eu malesuada odio. Sed facilisis sagittis mi et ornare. Vivamus finibus mollis nunc quis vestibulum. Pellentesque tempus blandit dictum. Nulla sem nibh, pretium in urna a, tristique ultrices felis. Morbi vel justo mi. Sed vel lobortis tortor. Nulla interdum nibh vel urna vulputate, sit amet viverra nunc tincidunt.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div class="list-advantages">
+      <div class="row justify-content-between">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+          <?php
+          $icon = "watch";
+          $title = "Hàng chính hãng";
+          $des = "Hiện nay, đồng hồ là phụ kiện thời trang thiết yếu đối với những người đàn ông hiện đại ngày nay.";
+          require("cvIntroPage.php");
+          ?>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+          <?php
+          $icon = "new_releases";
+          $title = "Sản phẩm mới 100%";
+          $des = "Hiện nay, đồng hồ là phụ kiện thời trang thiết yếu đối với những người đàn ông hiện đại ngày nay.";
+          require("cvIntroPage.php");
+          ?>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+          <?php
+          $icon = "settings_suggest";
+          $title = "Bảo hành 12 tháng";
+          $des = "Hiện nay, đồng hồ là phụ kiện thời trang thiết yếu đối với những người đàn ông hiện đại ngày nay.";
+          require("cvIntroPage.php");
+          ?>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+          <?php
+          $icon = "history";
+          $title = "Đổi trả trong vòng 7 ngày";
+          $des = "Hiện nay, đồng hồ là phụ kiện thời trang thiết yếu đối với những người đàn ông hiện đại ngày nay.";
+          require("cvIntroPage.php");
+          ?>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+          <?php
+          $icon = "local_shipping";
+          $title = "Miễn phí giao hàng";
+          $des = "Hiện nay, đồng hồ là phụ kiện thời trang thiết yếu đối với những người đàn ông hiện đại ngày nay.";
+          require("cvIntroPage.php");
+          ?>
+        </div>
+        <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+          <?php
+          $icon = "price_check";
+          $title = "Giá cả hợp lí";
+          $des = "Hiện nay, đồng hồ là phụ kiện thời trang thiết yếu đối với những người đàn ông hiện đại ngày nay.";
+          require("cvIntroPage.php");
+          ?>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
