46bf7e9ea4a736a0449656d26bf9cfc54a3ee42c
LinhBKU <linh.huynh.dev.99@gmail.com>
2021-11-22 20:33:22 +0700

    add menu

--- views/components/watch/filter/menu.php
+++ views/components/watch/filter/menu.php
@@ -1,1 +1,44 @@
-<div>menu</div>
+<div>
+  <div style="padding: 0 0 20 0;">
+    <div class="row">
+      <div class="col-9" style="display: flex; align-items: center; justify-content: flex-start;">
+        <h6>Thể loại</h6>
+      </div>
+      <div class="col-3" style="display: flex; align-items: center; justify-content: flex-end;">
+        <button class="btn btn-dark" style="padding: 5;" data-mdb-toggle="collapse" data-mdb-target="#collapseCategory" aria-expanded="false" aria-controls="collapseCategory">
+          <i class="fas fa-align-justify"></i>
+        </button>
+      </div>
+    </div>
+    <!-- Collapsed content -->
+    <div class="collapse mt-2" id="collapseCategory">
+      <!-- Default checkbox -->
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+        <label class="form-check-label" for="flexCheckDefault">
+          Default checkbox
+        </label>
+      </div>
+
+      <!-- Checked checkbox -->
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked />
+        <label class="form-check-label" for="flexCheckChecked">
+          Checked checkbox
+        </label>
+      </div>
+    </div>
+  </div>
+  </hr>
+  <div class="row">
+    <div class="col-9" style="display: flex; align-items: center; justify-content: flex-start;">
+      <h6>Giá</h6>
+    </div>
+    <div class="col-3" style="display: flex; align-items: center; justify-content: flex-end;">
+      <button class="btn btn-dark" style="padding: 5;" data-mdb-toggle="collapse" data-mdb-target="#collapseCategory" aria-expanded="false" aria-controls="collapseCategory">
+        <i class="fas fa-align-justify"></i>
+      </button>
+    </div>
+  </div>
+
+</div>
