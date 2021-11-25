d26e71a6d69cd4b0b136797380606d7e65d10195
vietnguyen2000 <nguyen.viet.21042000@gmail.com>
2021-11-25 08:48:35 +0700

    Revert "."
    This reverts commit f183c3b9fa2ce61466829c52ca4bbd3e6d85785f.

---
+++ views/components/footer/footerHTML.php
@@ -0,0 +1,36 @@
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"></script>
+
+  <?php if (isset($_SESSION['user']) && !$_SESSION['isRemembered']) { ?>
+    <script>
+      setTimeout(() => {
+        alert('Your session is out of time! Please login again!')
+        $.get('session-destroy')
+        location.reload()
+      }, <?= $session_time * 1000 ?>)
+    </script>
+  <?php } ?>
+
+  <script>
+    function addToCart(productId, productTitle) {
+      <?php if (!isset($_SESSION['user'])) { ?>
+        window.location.href = "<?= ROOT_URL . '/login' ?>"
+        return
+      <?php } else { ?>
+        $.post('/cart/add', {
+          productId
+        })
+        $.showNotification({
+          type: "primary",
+          body: "Bạn đã thêm \"" + productTitle + "\" vào giỏ hàng thành công",
+          duration: 3000,
+          direction: 'append'
+        })
+        $('.cart-badge').each((index, e) => {
+          e.innerHTML = parseInt(e.innerHTML) + 1
+        })
+      <?php } ?>
+    }
+  </script>
+</body>
+
+</html>
