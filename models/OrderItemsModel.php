d36c9bd302e611e4a4e08b81542bdd40a54c945c
huynhnam249 <67179307+huynhnam249@users.noreply.github.com>
2021-11-25 07:35:14 +0700

    add getByCondition

--- models/OrderItemsModel.php
+++ models/OrderItemsModel.php
@@ -9,43 +9,4 @@ class OrderItemsModel extends BaseModel
     parent::__construct();
     $this->name = 'orderItems';
   }
-
-  public function getByCondition(array ...$conditions)
-  {
-    try {
-      $name = 'orderItemsView';
-
-      $listSql = [];
-      $types = "";
-      $valuesList = [];
-
-      foreach ($conditions as $condition) {
-        $keysList = array_keys($condition);
-        $v = array_values($condition);
-        $keys = join("`= ? AND `", $keysList);
-        $cTypes = $this->getTypeBindParam($v);
-        $sql = "( `$keys` = ? )";
-        $listSql[] = $sql;
-        $types = $types . $cTypes;
-        array_push($valuesList, ...$v);
-      };
-
-      $conditionSql = join(" OR ", $listSql);
-
-      $sql = "SELECT * FROM $name WHERE $conditionSql";
-      $stmt = $this->db->prepare($sql);
-      $stmt->bind_param($types, ...$valuesList);
-      $stmt->execute();
-      $result =  $stmt->get_result();
-      $data = $result->fetch_all(mode: MYSQLI_ASSOC);
-      $data = array_map(function ($r) {
-        $r['imageURLs'] = explode('||', $r['imageURL']);
-        $r['imagePreview'] = $r['imageURLs'][0];
-        return $r;
-      }, $data);
-      return $data;
-    } catch (\Exception $e) {
-      return [];
-    }
-  }
 }
