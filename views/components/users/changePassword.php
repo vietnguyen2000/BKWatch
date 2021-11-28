ce86957824cb174062d9825250ba8f06a77382e8
LinhBKU <linh.huynh.dev.99@gmail.com>
2021-11-28 14:21:50 +0700

    doi mk

--- views/components/users/changePassword.php
+++ views/components/users/changePassword.php
@@ -1,27 +1,39 @@
-<div class="container mt-9 md-9">
-    <div class="row">
-        <div class="col-md-5">
+<div class="container">
+  <div style="margin: 50px 0;">
+    <div class="row justify-content-around">
+      <div class="col-10 col-sm-9 col-md-6 col-lg-5">
+        <div class="d-flex justify-content-center">
+          <div class="p-2 flex-fill">
             <h3 class="text-primary">Đổi mật khẩu</h3>
             <form method="POST" action="" id="formChangePass">
-                <div class="form-group">
-                    <label for="user_signin">Mật khẩu cũ</label>
-                    <input type="password" name="old_pass" class="form-control" id="old_pass">
-                </div>
-                <div class="form-group">
-                    <label for="user_signin">Mật khẩu mới</label>
-                    <input type="password" name="new_pass" class="form-control" id="new_pass">
-                </div>
-                <div class="form-group">
-                    <label for="user_signin">Nhập lại mật khẩu mới</label>
-                    <input type="password" name="re_new_pass" class="form-control" id="re_new_pass">
-                </div>
-                <a href="index.php" class="btn btn-default">
-                    <span class="glyphicon glyphicon-arrow-left"></span> Trở về
-                </a>
-                <button class="btn btn-primary" id="submit_change_pass" type="submit" name="btn-changepw">
+              <div class="form-group" style="margin: 0 0 25px 0;">
+                <label for="user_signin">Mật khẩu cũ</label>
+                <input type="password" name="old_pass" class="form-control" id="old_pass">
+              </div>
+              <div class="form-group" style="margin: 0 0 25px 0;">
+                <label for="user_signin">Mật khẩu mới</label>
+                <input type="password" name="new_pass" class="form-control" id="new_pass">
+              </div>
+              <div class="form-group" style="margin: 0 0 25px 0;">
+                <label for="user_signin">Nhập lại mật khẩu mới</label>
+                <input type="password" name="re_new_pass" class="form-control" id="re_new_pass">
+              </div>
+              <div class="d-flex flex-row-reverse">
+                <div class="p-2">
+                  <button class="btn btn-primary" id="submit_change_pass" type="submit" name="btn-changepw">
                     <span class="glyphicon glyphicon-ok"></span> Thay đổi
-                </button>
+                  </button>
+                </div>
+                <div class="p-2">
+                  <a href="/me" class="btn btn-default">
+                    <span class="glyphicon glyphicon-arrow-left"></span> Trở về
+                  </a>
+                </div>
+              </div>
             </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </div>
--- views/pages/users/userUpdateProfile.php
+++ views/pages/users/userUpdateProfile.php
@@ -1,115 +1,113 @@
-<div class="container mt-5 mb-5">
+<div class="container">
+  <div style="margin: 50px 0;">
     <form action="/user/<?php echo $user['id'] ?>/update" id="user-update-form" method="POST">
-        <!-- User Profile -->
-        <div class="card">
-            <div class="card-title">
-                <h2>Thông tin cá nhân</h2>
-            </div>        
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-12 col-lg-3">
-                        <?php require(realpath($_SERVER["DOCUMENT_ROOT"]) . '/views/components/users/userGeneral.php');?>
-                    </div>
-                    
 
-                    <div class="card col-12 col-lg-9">
-                    <div class="card-body">
-                    <?php require(realpath($_SERVER["DOCUMENT_ROOT"]) . '/views/components/users/userUpdate.php');?>
-                </div>
-                <div class="card-footer">
-                    <div class="btn-group" role="group" aria-label="">    
-                        <a href="#" class="btn btn-primary btn-submit" onClick="document.getElementById('user-update-form').submit()">Lưu</a>
-                        <a href="/me" class="btn btn-black">Huỷ</a>
-                    </div>
-                </div>
-                    </div>
-                </div>
-            </div>
+      <h2>Thông tin cá nhân</h2>
+
+      <div class="row">
+        <div class="col-12 col-lg-3">
+          <?php require(realpath($_SERVER["DOCUMENT_ROOT"]) . '/views/components/users/userGeneral.php'); ?>
         </div>
-    </form>
+
+
+        <div class="card col-12 col-lg-9">
+          <div class="card-body">
+            <?php require(realpath($_SERVER["DOCUMENT_ROOT"]) . '/views/components/users/userUpdate.php'); ?>
+          </div>
+          <div class="card-footer">
+            <div class="btn-group me-2 mb-2" role="group" aria-label="">
+              <a href="#" class="btn btn-primary btn-submit" onClick="document.getElementById('user-update-form').submit()">Lưu</a>
+            </div>
+            <div class="btn-group me-2 mb-2" role="group" aria-label="">
+              <a href="/me" class="btn btn-black">Huỷ</a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  </form>
 </div>
 <div id="uploadZone" hidden>
-    <!-- Background image -->      
-      <div class="mask" style="background-color: rgba(0, 0, 0, 0.8);" onclick="hideUploadZone()">
-        <div class="container d-flex align-items-center justify-content-center text-center h-100">
-            <div class="card dropzone-container">
-                <div class="card-body">
-                    <div class="dropzone" id="upload-profile-picture-dropzone" action="/image/upload">
-                        <input type="file" name="file" hidden/>
-                    </div>
-                    <div id="uploadMsg"></div>
-                    <div class="card-footer">
-                        <input class="btn btn-success float-end" id="upload-image" type="submit" value="Tải lên">
-                        <div class="clearfix"></div>
-                    </div>
-                </div>
-            </div>
+  <!-- Background image -->
+  <div class="mask" style="background-color: rgba(0, 0, 0, 0.8);" onclick="hideUploadZone()">
+    <div class="container d-flex align-items-center justify-content-center text-center h-100">
+      <div class="card dropzone-container">
+        <div class="card-body">
+          <div class="dropzone" id="upload-profile-picture-dropzone" action="/image/upload">
+            <input type="file" name="file" hidden />
+          </div>
+          <div id="uploadMsg"></div>
+          <div class="card-footer">
+            <input class="btn btn-success float-end" id="upload-image" type="submit" value="Tải lên">
+            <div class="clearfix"></div>
+          </div>
         </div>
       </div>
     </div>
-    <!-- Background image -->
+  </div>
+</div>
+<!-- Background image -->
 </div>
 
 <script>
-    Dropzone.options.uploadProfilePictureDropzone = { // camelized version of the `id`
-        paramName: "file", // The name that will be used to transfer the file
-        maxFiles: 1,
-        parallelUploads: 1,
-        addRemoveLinks: true,
-        maxFilesize: 2, // MB
-        thumbnailHeight: 250,
-        thumbnailHeight: 250,
-        resizeWidth: 250,
-        resizeHeight: 250,
-        thumbnailMethod: "contain",
-        accept: function(file, done) {
-            done();
+  Dropzone.options.uploadProfilePictureDropzone = { // camelized version of the `id`
+    paramName: "file", // The name that will be used to transfer the file
+    maxFiles: 1,
+    parallelUploads: 1,
+    addRemoveLinks: true,
+    maxFilesize: 2, // MB
+    thumbnailHeight: 250,
+    thumbnailHeight: 250,
+    resizeWidth: 250,
+    resizeHeight: 250,
+    thumbnailMethod: "contain",
+    accept: function(file, done) {
+      done();
+    },
+    maxfilesexceeded: function(file) {
+      this.removeFile(file);
+    }
+  };
+
+  function openUploadDropzone() {
+    document.getElementById("uploadZone").removeAttribute("hidden");
+  }
+
+  function hideUploadZone() {
+    document.getElementById('uploadZone').addEventListener('click', function(e) {
+      this.style.removeProperty('hidden');
+    });
+    // document.getElementById("uploadZone").setAttribute("hidden", "");
+  }
+
+  $(document).ready(function() {
+    document.getElementById("upload-image").addEventListener("click", function(event) {
+      event.preventDefault();
+
+      var dropzone = document.querySelector(".dropzone").dropzone;
+      var files = dropzone.getAcceptedFiles();
+      if (!(files && files.length > 0)) {
+        document.querySelector("#uploadMsg").innerHTML = "Chọn ảnh";
+        return;
+      }
+      var data;
+
+      data = new FormData();
+      data.append('image', files[0]);
+      $.ajax({
+        url: '/image/upload',
+        data: data,
+        processData: false,
+        contentType: false,
+        type: 'POST',
+        success: function(data) {
+          document.getElementById('uploadMsg').innerHTML = "Cập nhật thành công";
+          location.reload(); // reload
         },
-        maxfilesexceeded: function(file) {
-            this.removeFile(file);
+        error: function(xhr, status, error) {
+          document.getElementById('uploadMsg').innerHTML = xhr.responseText;
         }
-    };
-
-    function openUploadDropzone() {
-        document.getElementById("uploadZone").removeAttribute("hidden");
-    }
-
-    function hideUploadZone() {
-        document.getElementById('uploadZone').addEventListener('click', function (e) {
-        this.style.removeProperty('hidden');
+      });
     });
-        // document.getElementById("uploadZone").setAttribute("hidden", "");
-    }
-
-    $(document).ready(function() {
-        document.getElementById("upload-image").addEventListener("click", function(event) {
-            event.preventDefault();
-            
-            var dropzone = document.querySelector(".dropzone").dropzone;
-            var files = dropzone.getAcceptedFiles();
-            if (!(files && files.length > 0)) {
-                document.querySelector("#uploadMsg").innerHTML = "Chọn ảnh";
-                return;
-            }
-            var data;
-
-            data = new FormData();
-            data.append( 'image', files[0]);
-            $.ajax({
-                url: '/image/upload',
-                data: data,
-                processData: false,
-                contentType: false,
-                type: 'POST',
-                success: function ( data ) {
-                    document.getElementById('uploadMsg').innerHTML = "Cập nhật thành công";
-                    location.reload(); // reload
-                },
-                error: function(xhr, status, error) {
-                    document.getElementById('uploadMsg').innerHTML = xhr.responseText;
-                }
-            });
-        });
-    });
-    
+  });
 </script>
