78a147276e4286eeac113063af75aa0bd2b3eca3
vietnguyen2000 <nguyen.viet.21042000@gmail.com>
2021-11-30 00:00:47 +0700

    fix after merge

--- views/admin/adminUser.php
+++ views/admin/adminUser.php
@@ -8,20 +8,20 @@
 </section>
 
 
-  <section class="section main-section">
-    <div class="card has-table">
-      <header class="card-header">
-        <p class="card-header-title">
-          <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
-          User
-        </p>
-        <a href="#" class="card-header-icon">
-          <span class="icon"><i class="mdi mdi-reload"></i></span>
-        </a>
-      </header>
-      <div class="card-content">
-        <table id="table">
-          <thead>
+<section class="section main-section">
+  <div class="card has-table">
+    <header class="card-header">
+      <p class="card-header-title">
+        <span class="icon"><i class="mdi mdi-account-multiple"></i></span>
+        User
+      </p>
+      <a href="#" class="card-header-icon">
+        <span class="icon"><i class="mdi mdi-reload"></i></span>
+      </a>
+    </header>
+    <div class="card-content">
+      <table id="table">
+        <thead>
           <tr>
             <th>ID</th>
             <th>User Name</th>
@@ -32,42 +32,47 @@
             <th>Create at</th>
             <th></th>
           </tr>
-          </thead>
-          <tbody>
-          <?php  foreach ($data['data'] as $row) {?>
-            <tr id="user-<?php echo($row["id"]); ?>">
-                <td data-label="ID"><?php echo($row["id"]); ?></td>
-                <td data-label="User Name"><?php echo($row["username"]); ?></td>
-                <td data-label="FName"><?php echo($row["fullname"]); ?></td>
-                <td data-label="Email"><?php echo($row["email"]); ?></td>
-                <td data-label="Phone"><?php echo($row["phoneNumber"]); ?></td>
-                <td data-label="Role">
-                  <form action="">
-                  <input class="input" type="number"  name="userRole<?php echo($row["id"]); ?>" required value="<?php echo($row["role"]); ?>">
-                  </form>
-                </td>                
-                <td data-label="Created">
-                <small class="text-gray-500"><?php echo($row["createdAt"]); ?></small>
-                </td>
-                <td class="actions-cell">
-                  <form form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-                    <div class="buttons right nowrap">
-                        <button class="button small green --jb-modal"  data-target="sample-modal-2" type="button" onclick="updateID('<?php echo($row["id"]); ?>')">
-                        <span class="icon"><i class="mdi mdi-square-edit-outline"></i></span>
-                        </button>
-                        <button class="button small red --jb-modal" data-target="sample-modal" type="button" onclick="deleteID('<?php echo($row["id"]); ?>')">
-                        <span class="icon"><i class="mdi mdi-trash-can"></i></span>
-                        </button>
-                    </div>
-                  </form>
-                </td>
+        </thead>
+        <tbody>
+          <?php foreach ($data['data'] as $row) { ?>
+            <tr id="user-<?php echo ($row["id"]); ?>">
+              <td data-label="ID"><?php echo ($row["id"]); ?></td>
+              <td data-label="User Name"><?php echo ($row["username"]); ?></td>
+              <td data-label="FName"><?php echo ($row["fullname"]); ?></td>
+              <td data-label="Email"><?php echo ($row["email"]); ?></td>
+              <td data-label="Phone"><?php echo ($row["phoneNumber"]); ?></td>
+              <td data-label="Role">
+                <form action="">
+                  <div class="select">
+                    <select class="input" type="number" name="userRole<?php echo ($row["id"]); ?>" required>
+                      <option value="0" <?= ($row["role"] == 0) ? 'selected' : '' ?>> Member</option>
+                      <option value="1" <?= ($row["role"] == 1) ? 'selected' : '' ?>> Admin</option>
+                    </select>
+                  </div>
+                </form>
+              </td>
+              <td data-label="Created">
+                <small class="text-gray-500"><?php echo ($row["createdAt"]); ?></small>
+              </td>
+              <td class="actions-cell">
+                <form form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                  <div class="buttons right nowrap">
+                    <button class="button small green --jb-modal" data-target="sample-modal-2" type="button" onclick="updateID('<?php echo ($row["id"]); ?>')">
+                      <span class="icon"><i class="mdi mdi-content-save-outline"></i></span>
+                    </button>
+                    <button class="button small red --jb-modal" data-target="sample-modal" type="button" onclick="deleteID('<?php echo ($row["id"]); ?>')">
+                      <span class="icon"><i class="mdi mdi-trash-can"></i></span>
+                    </button>
+                  </div>
+                </form>
+              </td>
             </tr>
-          <?php }?>
-          </tbody>
-        </table>
-      </div>
+          <?php } ?>
+        </tbody>
+      </table>
     </div>
-  </section>
+  </div>
+</section>
 
 
 
@@ -106,43 +111,62 @@
 
 
 <script>
-  
   var ID = 0;
   var modal1 = document.getElementById("sample-modal-1");
   var modal = document.getElementById("sample-modal-2");
   // When the user clicks the button, open the modal 
-  function deleteID(id){
+  function deleteID(id) {
     modal.style.display = "block";
     ID = id;
   }
-  function updateID(id){
+
+  function updateID(id) {
     modal1.style.display = "block";
     ID = id;
   }
-  function cancel(){
+
+  function cancel() {
     modal.style.display = "none";
   }
-  function cancel1(){
+
+  function cancel1() {
     modal1.style.display = "none";
   }
-  function Update(){
-    var role = document.getElementsByName("userRole"+ID)[0].value;
+
+  function Update() {
+    var role = document.getElementsByName("userRole" + ID)[0].value;
     console.log(ID, role);
     $.post('/cmsUser/update', {
-          ID, role
-        })
+      ID,
+      role
+    }, () => {
+      toastsHandler.createToast({
+        type: "success",
+        icon: "check-circle",
+        message: "Bạn đã cập nhật thành công",
+        duration: 3000,
+      })
+    })
     cancel1();
     return true;
   }
-  function Delete(){
+
+  function Delete() {
     $(`tr[id="user-${ID}"]`).remove();
     $.post('/cmsUser/delete', {
-          ID
-        })
+      ID
+    }, () => {
+      toastsHandler.createToast({
+        type: "success",
+        icon: "check-circle",
+        message: "Bạn đã xóa thành công",
+        duration: 3000,
+      })
+    })
     cancel();
     return true;
   }
-  $(document).ready(function () {
-      $('#table').DataTable();
+  $(document).ready(function() {
+    $('#table').DataTable();
   });
 </script>
